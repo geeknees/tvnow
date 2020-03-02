aaff3e4b182f834354dde3e3c3bf401f3ca1b282
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2020-03-02 22:45:25 +0900

    Fix spec

--- spec/tvnow_spec.rb
+++ spec/tvnow_spec.rb
@@ -5,7 +5,7 @@ describe Tvnow do
     expect(Tvnow::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'retruns array' do
+    expect(Tvnow::Client.new.get_program_list).to be_an_instance_of(Array)
   end
 end
